3bdd46b91ec2f3e15c7251b04a86baa002cd4e4d
Ольга Шполянская <olgab497@gmail.com>
2023-05-11 18:32:55 +0300

    User urls added

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,22 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
-router = DefaultRouter()
+from .views import (APIToken, APISignup, CategoryViewSet, CommentViewSet,
+                    GenreViewSet, ReviewViewSet, TitleViewSet, UsersViewSet)
+
+app_name = 'api'
+
+router = SimpleRouter()
+
+router.register(
+    'users',
+    UsersViewSet,
+    basename='users'
+)
+
 
 urlpatterns = [
+    path('v1/auth/token/', APIToken.as_view(), name='api_token'),
     path('v1/', include(router.urls)),
+    path('v1/auth/signup/', APISignup.as_view(), name='api_signup'),
 ]
