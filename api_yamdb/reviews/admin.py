9223062d23047c4c6b7fc6fbd9ca34533785cad4
Krivov Aleksandr <krivov.av91@gmail.com>
2023-05-11 18:45:00 +0700

    added models and admin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -2,3 +2,16 @@ from django.contrib import admin
 
 from .models import Category, Comment, Genre, GenreTitle, Review, Title, User
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text',  'author', 'review')
+
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'author', 'score')
+
+
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)    
+
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -26,8 +26,56 @@ class GenreTitle(models.Model):
 
 
 class Review(models.Model):
-    pass
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='произведение'
+    )
+    text = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='автор'
+    )
+
+    score = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+    def __str__(self):
+        return self.text
+    
+    
 
 
 class Comment(models.Model):
-    pass
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='coments',
+        verbose_name='отзыв'
+    )
+    text = models.CharField(
+        'техт коментария',
+        max_length=200
+        
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='coments',
+        verbose_name='автор'
+    )
+
+    class Meta:
+        verbose_name = 'Коментарий'
+        verbose_name_plural = 'Коментарий'
+
+    def __str__(self):
+        return self.text    
+
+
