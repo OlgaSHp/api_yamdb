4968a19433542613dd14dbef4a13ef0b48d90047
Андрей Анпилов <anpilovav85@yandex.ru>
2023-05-11 16:00:07 +0300

    add serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from reviews.models import Category, CHOICES, Genre, Title
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -7,19 +7,50 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Category
+        fields = '__all__'
 
 
 class GenreSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Genre
+        fields = '__all__'
 
 
 class TitleSerializer(serializers.ModelSerializer):
-    pass
+    category = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(),
+        slug_field='category')
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(),
+        many=True,
+        slug_field='genre')
+
+    class Meta:
+        model = Title
+        fields = '__all__'
+
+# Или так:
+
+# class TitleSerializer(serializers.ModelSerializer):
+#     category = CategorySerializer()
+#     genre = GenreSerializer(many=True)
+
+#     class Meta:
+#         model = Title
+#         fields = '__all__'
 
 
 class ReadOnlyTitleSerializer(serializers.ModelSerializer):
-    pass
+    category = serializers.StringRelatedField()
+    genre = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Title
+        fields = '__all__'
+        read_only_fields = ('id', 'name', 'year',
+                            'description', 'category', 'genre')
 
 
 class UserSerializer(serializers.ModelSerializer):
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -8,37 +8,58 @@ class User(AbstractUser):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=256)
     slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=256)
     slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
 
     def __str__(self):
         return self.name
 
 
 class Title(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=256)
     year = models.IntegerField()
-    # вопрос по удалению связанных данных
-    category_id = models.ForeignKey(
-        Category, related_name='titles')
+    description = models.TextField()
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, related_name='titles')
     genre = models.ManyToManyField(
         Genre, through='GenreTitle')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
 
     def __str__(self):
         return self.name
 
 
 class GenreTitle(models.Model):
-    title_id = models.ForeignKey(Title, on_delete=models.CASCADE)
-    genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Жанр произведения'
+
+    def __str__(self):
+        return f'{self.title}, жанр - {self.genre}'
 
 
 class Review(models.Model):
