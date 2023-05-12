73605b3abd88cb48ea435dc3c95dd8a6a009988f
Андрей Анпилов <anpilovav85@yandex.ru>
2023-05-12 11:01:54 +0300

    Добавление админки

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -2,16 +2,14 @@ from django.contrib import admin
 
 from .models import Category, Comment, Genre, Review, Title, User
 
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'text',  'author', 'review')
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'author', 'review')
 
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('id', 'text', 'author', 'score')
 
 
-
 admin.site.register(Review, ReviewAdmin)
-admin.site.register(Comment, CommentAdmin)    
-
+admin.site.register(Comment, CommentAdmin)
